2c6b0054a9f7c44ad6b552700fb65159dd7f6060
hutson <hotson.lu@hotmail.com>
2025-02-13 18:31:54 +0800

    home change

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -1,3 +1,13 @@
 <template>
-
+  <div
+    class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
+  >
+    <h2 class="text-4xl tracking-tight text-white md:text-6xl">
+      URL Shortening
+    </h2>
+    <p class="mt-4 text-lg text-slate-400 md:text-xl">
+      Compress your URLs to their minimal length. <br>
+      Monitor link analytics and gather insightful statistics.    
+    </p>  
+  </div>
 </template>
